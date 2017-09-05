0637f355072f80f51e388787e6ce4b902e3a955b
Ewan <Ewan@192.168.0.22>
2017-09-05 08:21:03 -0400

    Init

---
+++ FastImaging/Imaging/Imaging/FastImaging.java
@@ -0,0 +1,288 @@
+package Imaging;
+
+import java.awt.FileDialog;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.imageio.ImageIO;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class FastImaging extends JPanel implements Runnable,KeyListener{
+	static BufferedImage i;
+	static BufferedImage[] i2 = new BufferedImage[4];
+	static ArrayList<Integer> a = new ArrayList<Integer>();
+	static int cooldown = 0;
+	static int maxCooldown = 20;
+	static boolean reverse = false;
+	static int w = 0;
+	static int h = 0;
+	static int ticks = 0;
+	static Random rand = new Random();
+	static boolean paused = false;
+	static int[][][] RGBs;
+	static int[][][] originalRGBs;
+	static boolean[] keySet = new boolean[256];
+	static ExecutorService e = Executors.newFixedThreadPool(4);
+	public static BufferedImage scale( BufferedImage sbi,  int imageType,  int dWidth,  int dHeight,  double fWidth,  double fHeight) {
+		BufferedImage dbi = null;
+		if(sbi != null) {
+			dbi = new BufferedImage(dWidth, dHeight, imageType);
+			Graphics2D g = dbi.createGraphics();
+			AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
+			g.drawRenderedImage(sbi, at);
+		}
+		return dbi;
+	}
+	public static void main(String[] args) throws InterruptedException{
+		System.setProperty("sun.java2d.opengl","True");
+		FileDialog fd = new FileDialog((java.awt.Frame) null);
+		fd.setTitle("Choose an image");
+		fd.setVisible(true);
+		File f = new File(fd.getDirectory() + fd.getFile());
+		if(fd.getDirectory() == null || fd.getFile() == null)
+			System.exit(0);
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(f);
+			img.getType();
+		} catch (IOException | NullPointerException e) {}
+		i = img;
+		w = i.getWidth();
+		h = i.getHeight();
+		int mW = 200;
+		if(w > mW){
+			JOptionPane infoPane = new JOptionPane();
+			infoPane.setMessage("Image is too big, do you want it resized?");
+			infoPane.setMessageType(JOptionPane.INFORMATION_MESSAGE);
+			infoPane.setOptionType(JOptionPane.PLAIN_MESSAGE);
+			JDialog infoDialog = infoPane.createDialog(new JFrame(), "Uh Oh");
+			infoDialog.setResizable(true);
+			infoDialog.setModal(true);
+			Object[] options = { "OK!", "NO!","EXIT?" };
+			infoPane.setOptions(options);
+			infoDialog.setSize(320,200);
+			infoDialog.setVisible(true);
+			if(infoPane.getValue() == "EXIT?" || infoPane.getValue() == null){
+				System.exit(0);
+			}
+			if(infoPane.getValue() == "OK!"){
+				double w1 = i.getWidth();
+				double h1 = i.getHeight();
+				double w2 = mW;
+				double ratio = (double) w1 / (double) w2;
+				double h2 = h1 / ratio;
+				i = scale(img, img.getType(), mW,(int)h2, 1D / ratio, 1D / ratio);
+			}
+			w = i.getWidth();
+			h = i.getHeight();
+
+		}
+		JFrame fr = new JFrame();
+		FastImaging god = new FastImaging();
+		god.addKeyListener(god);
+		god.setFocusable(true);
+		god.setSize(1920,1080);
+		fr.add(god);
+		fr.pack();
+		fr.setSize(1000, 1000);
+		fr.setVisible(true);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Thread t1 = new Thread(god);
+		t1.start();
+		for(int i = 0; i < 4;i++){
+			i2[i] = new BufferedImage(w / 2,h / 2,BufferedImage.TYPE_3BYTE_BGR);
+		}
+		RGBs = new int[w][h][3];
+		originalRGBs = new int[w][h][3];
+		for(int x = 0; x < w;x++){
+			for(int y = 0; y < h;y++){	
+				int rgb = i.getRGB(x, y);
+				int r = rgb >> 16 & 0XFF;
+			int g = rgb >> 8 & 0XFF;
+		int b = rgb & 0XFF;
+		RGBs[x][y][0] = r;
+		RGBs[x][y][1] = g;
+		RGBs[x][y][2] = b;
+		originalRGBs[x][y][0] = r;
+		originalRGBs[x][y][1] = g;
+		originalRGBs[x][y][2] = b;
+
+			}
+		}
+		while(true){
+			update();
+			doImages();
+		}
+	}
+	//	int rgb = r << 16 | g << 8 | b ;
+	//	int r = rgb >> 16 & 0XFF;
+	//	int g = rgb >> 8 & 0XFF;
+	//	int b = rgb & 0XFF;
+	//	static BufferedImage deepCopy(BufferedImage bi){
+	//		ColorModel cm = bi.getColorModel();
+	//		boolean iap = cm.isAlphaPremultiplied();
+	//		WritableRaster raster = bi.copyData(null);
+	//		return new BufferedImage(cm,raster,iap,null);
+	//	}
+	public static void update(){
+		ticks++;
+		cooldown--;
+		//Multithread this too?
+		for(int x = 0; x < w;x++){
+			for(int y = 0; y < h;y++){	
+				if(keySet[KeyEvent.VK_P]){
+					if(x % 3 > 1 || y % 3 > 1){
+						continue;
+					}
+				}
+				if(keySet[KeyEvent.VK_O]){
+					int m = 0;
+					for(int i = 1; i < 3;i++){
+						int l = RGBs[x][y][i];
+						if(l > RGBs[x][y][m]){
+							m = i;
+						}
+					}
+					for(int i = 0; i < 3;i++){
+						if(i == m){
+							RGBs[x][y][i] = 255;
+						}
+						else{
+							RGBs[x][y][i] = 0;
+						}
+					}
+				}
+				if(keySet[KeyEvent.VK_R]){
+					for(int i = 0; i < 3;i++){
+						int d = originalRGBs[x][y][i] - RGBs[x][y][i];
+						int s = (int) Math.signum(d);
+						if(Math.abs(d) > 3){
+							RGBs[x][y][i] += s * 3;
+						}
+						else{
+							RGBs[x][y][i] += s;
+						}
+					}
+				}
+				else if(!paused){
+					int s = rand.nextInt(3);
+					s = 1;
+					int X = rand.nextInt(5) - 2;
+					int Y = rand.nextInt(5) - 2;
+					X = 0;
+					Y = 0;
+					if(keySet[KeyEvent.VK_UP]){
+						Y =  1;
+					}
+					if(keySet[KeyEvent.VK_DOWN]){
+						Y = -1;
+					}
+					if(keySet[KeyEvent.VK_LEFT]){
+						X = 1;
+					}
+					if(keySet[KeyEvent.VK_RIGHT]){
+						X = -1;
+					}
+					if(keySet[KeyEvent.VK_CONTROL]){
+						X *= 3;
+						Y *= 3;
+					}
+					if(keySet[KeyEvent.VK_SHIFT]){
+						X *= 2;
+						Y *= 2;
+					}
+					int nX = x + X;
+					int nY = y + Y;
+					int c = 1000;
+					//					if(keySet[KeyEvent.VK_B]){
+					//						c -= 8;
+					//					}
+					//					if(keySet[KeyEvent.VK_V]){
+					//						c += 40;
+					//					}
+					if((rand.nextInt(100) < c)){
+						if(nX < 0){
+							nX = w + nX;
+						}
+						if(nY < 0){
+							nY = h + nY;
+						}
+						if(nX >= w){
+							nX = nX % w;
+						}
+						if(nY >= h){
+							nY = nY % h;
+						}
+						RGBs[x][y][s] = RGBs[nX][nY][s];
+						RGBs[nX][nY][s] = RGBs[x][y][s];
+					}
+				}
+			}
+
+
+		}
+
+
+	}
+	public static void doImages(){
+		//		long t0 = System.nanoTime();
+		//		for(int i = 0; i < 10;i++){
+		Future<BufferedImage> f1 = e.submit(new Worker(w/2, h/2, 0,   0));
+		Future<BufferedImage> f2 = e.submit(new Worker(w/2, h/2, w/2, 0));
+		Future<BufferedImage> f3 = e.submit(new Worker(w/2, h/2, 0,   h/2));
+		Future<BufferedImage> f4 = e.submit(new Worker(w/2, h/2, w/2, h/2));
+		while(!(f1.isDone() && f2.isDone() && f3.isDone() && f4.isDone())){
+
+		}
+		try {
+			i2[0] = f1.get();
+			i2[1] = f2.get();
+			i2[2] = f3.get();
+			i2[3] = f4.get();
+		} catch (InterruptedException | ExecutionException e1) {
+			e1.printStackTrace();
+		}
+		//		}
+		//		long t1 = System.nanoTime();
+		//		System.out.println(t1 - t0);
+	}
+	public void paint(Graphics gr){
+		super.paint(gr);
+
+		gr.drawImage(i2[0], 0, 0, null);
+		gr.drawImage(i2[1], w/2, 0, null);
+		gr.drawImage(i2[2], 0, h/2, null);
+		gr.drawImage(i2[3], w/2, h/2, null);
+
+
+	}
+	@Override
+	public void run() {
+		while(true) {
+			repaint();
+			if(keySet[KeyEvent.VK_SPACE] && cooldown < 0){
+				cooldown = maxCooldown;
+				paused = !paused;
+			}
+		}
+	}
+	public void keyTyped(KeyEvent e) {
+	}
+	public void keyPressed(KeyEvent e) {keySet[e.getKeyCode()] = true;}
+	public void keyReleased(KeyEvent e) {keySet[e.getKeyCode()] = false;}
+}
---
+++ FastImaging/Imaging/Imaging/Worker.java
@@ -0,0 +1,40 @@
+package Imaging;
+
+import static Imaging.FastImaging.RGBs;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Callable;
+public class Worker implements Callable<BufferedImage>{
+
+	//position and offset + dimenson
+	int w = 0;
+	int h = 0;
+	int x = 0;
+	int y = 0;
+	BufferedImage bi;
+	public Worker(int w, int h, int x, int y){
+		this.w = w;
+		this.h = h;
+		this.x = x;
+		this.y = y;
+		bi = new BufferedImage(w,h,BufferedImage.TYPE_3BYTE_BGR);
+	}
+	
+	public BufferedImage call() throws Exception {
+		//return image
+		Graphics g = bi.getGraphics();
+		for(int i = 0; i < w;i++){
+			for(int j = 0; j < h;j++){
+				int X = i + x;
+				int Y = j + y;
+				
+				g.setColor(new Color(RGBs[X][Y][0],RGBs[X][Y][1],RGBs[X][Y][2]));
+				g.fillRect(i, j, 1, 1);
+			}
+		}
+		return bi;
+	}
+
+}
